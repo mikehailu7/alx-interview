bb673667deb4e5f036b5ab7be8ffe7d3c5f1e59c
mikehailu7 <mikehailu7@gmail.com>
2024-03-06 22:43:55 -0800

    hot fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,25 +2,24 @@
 #Author: MikiasHailu
 """Pascal Triangle"""
 
-
 def pascal_triangle(n):
-    """this is a pascal trinagle 
-    represent with n sides"""
-    if n <= 0:
-        return []
+	"""this is a pascal trinagle 
+		represent with n sides"""
+		if n <= 0:
+		return []
 
-    pascal_t = []
+		pascal_t = []
 
-    for m in range(n):
-        pascal_t.append([])
-        pascal_t[m].append(1)
+		for m in range(n):
+	pascal_t.append([])
+	pascal_t[m].append(1)
 
-        for j in range(1, m):
-            x = pascal_t[m-1][j-1]
-            y = pascal_t[m-1][j]
-            pascal_t[m].append(x+y)
+	for j in range(1, m):
+		x = pascal_t[m-1][j-1]
+	y = pascal_t[m-1][j]
+	 pascal_t[m].append(x+y)
 
-        if(n != 0 and m != 0):
-            pascal_t[m].append(1)
+	 if(n != 0 and m != 0):
+		 pascal_t[m].append(1)
 
-    return pascal_t
+		 return pascal_t
