f516b0c67beb27c7b376b88d26a0ae2b2576b712
mikehailu7 <mikehailu7@gmail.com>
2024-03-17 22:57:02 -0700

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Author: MikiasHailu
+"""alx interveiw minimum oper"""
+
+
+def primeFactorization(x):
+	""" This function will return  prime factorization of x"""
+	div = 2
+	array = list()
+	while (div <= x):
+		if x % div == 0:
+		array.append(div)
+		x /= div
+		else:
+		div += 1
+
+		return array
+
+
+		def minOperations(n):
+			""" This function will caluclate the smallest number"""
+				min = 0
+				factors = [x for x in primeFactorization(n)]
+				occurences = {item: factors.count(item) for item in factors}
+				for k, v in occurences.items():
+					min += k * v
+					return min
