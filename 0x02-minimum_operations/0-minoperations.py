dd5b78a860f1914c1565ce3e829197abbad3b0c9
mikehailu7 <mikehailu7@gmail.com>
2024-03-20 22:49:43 -0700

    easy fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 # Author: MikiasHailu
 """alx interveiw minimum oper"""
-
 def primeFactorization(x):
-	""" This function will return  prime factorization of x"""
-	div = 2
-	array = list()
-	while (div <= x):
-		if x % div == 0:
-		array.append(div)
-		x /= div
-		else:
-		div += 1
+    """ This function will return prime factorization elements of x"""
+    div = 2
+    array = list()
+    while (div <= x):
+        if x % div == 0:
+            array.append(div)
+            x /= div
+        else:
+            div += 1
 
-		return array
+    return array
 
 
-		def minOperations(n):
-			""" This function will caluclate the smallest number"""
-				min = 0
-				factors = [x for x in primeFactorization(n)]
-				occurences = {item: factors.count(item) for item in factors}
-				for k, v in occurences.items():
-					min += k * v
-					return min
+def minOperations(n):
+    """ This function will calculate the fewest number of operations 
+	that are needed"""
+    min = 0
+    factors = [x for x in primeFactorization(n)]
+    occurences = {item: factors.count(item) for item in factors}
+    for k, v in occurences.items():
+        min += k * v
+    return min
